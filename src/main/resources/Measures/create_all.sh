7c89919dd01ec493a882c97b08655e0b7e7d517e
matthias <Matthias.Zober1993@outlook.de>
2016-01-13 01:32:35 +0100

    Remove MemberFinder and unnecessary files

--- src/main/resources/Measures/create_all.sh
+++
@@ -1,4 +0,0 @@
-gnuplot -e "filename='measure_10'" -e "output_format='$1'" measure.plt && \
-gnuplot -e "filename='measure_100'" -e "output_format='$1'" measure.plt  && \
-gnuplot -e "filename='measure_1000'" -e "output_format='$1'" measure.plt && \
-gnuplot -e "filename='measure_10000'" -e "output_format='$1'" measure.plt
